7b03fab4be6a34c3d415e7831d9dbc6856ef6e2e
Mehmet Salih <joker16131613@gmail.com>
2025-09-30 16:10:38 +0300

    1’den 100’e kadar sayılar için 3’e bölünenlerde “Fizz”, 5’e bölünenlerde “Buzz”, her ikisine bölünenlerde “FizzBuzz” yazdır.

---
+++ Test-5 FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_5_FizzBuzz
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(" ---------- FizzBuzz ---------- ");
+
+			int Number = 1;
+
+			while (Number <= 100)
+			{
+				 if ((Number % 3) == 0 && (Number % 5) == 0)
+				{
+					Console.WriteLine(Number + " Sayısı FizzBuzz"); 
+				}
+
+				else if ( (Number % 3)  == 0)
+				{
+					Console.WriteLine(Number + " Sayısı Fizz"); 
+				}
+				else if ((Number % 5) == 0)
+				{
+					Console.WriteLine(Number + " Sayısı Buzz"); 
+				}
+				else
+				{
+					Console.WriteLine(Number);
+				}
+				Number++;
+			}
+		}
+	}
+}
