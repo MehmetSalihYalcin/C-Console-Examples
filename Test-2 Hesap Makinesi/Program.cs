72909df350b0cd48339dc4dd1fea9e1ffa5ed702
Mehmet Salih <joker16131613@gmail.com>
2025-09-30 14:58:27 +0300

    Test-2

---
+++ Test-2 Hesap Makinesi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2_Hesap_Makinesi
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" --------- Hesap Makinesi --------- ");
+			Console.Write("İlk Sayınız : ");
+			int Number_1 = int.TryParse(Console.ReadLine(), out int num1) ? num1 : 0;
+			Console.Write("İkinci Sayınız : ");
+			int Number_2 = int.TryParse(Console.ReadLine(), out int num2) ? num2 : 0;
+			Console.WriteLine("Hangi işlemi yapmak istersiniz (+, -, *, /) : ");
+			string Process = Console.ReadLine();
+
+			switch (Process)
+			{
+				case "+":
+					Console.Write("İki sayınızın toplamı : " + (Number_1 + Number_2));
+					break;
+
+				case "-":
+					Console.Write("İki sayınızın farkı : " + (Number_1 - Number_2));
+					break;
+
+				case "*":
+					Console.Write("İki sayınızın çarpımı : " + (Number_1 * Number_2));
+					break;
+
+				case "/":
+					Console.Write("İki sayınızın bölümü : " + (Number_1 / Number_2));
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
+}
---
+++ Test-2 Hesap Makinesi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-2 Hesap Makinesi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-2 Hesap Makinesi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0493fb13-bbf2-4c50-8e05-b337dcbc0dea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
