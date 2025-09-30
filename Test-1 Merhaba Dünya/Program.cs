d944a6b78abc33d095ceacf3abdc6bfb37390c43
Mehmet Salih <joker16131613@gmail.com>
2025-09-30 14:27:11 +0300

    Add project files.

---
+++ Test-1 Merhaba Dünya/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1_Merhaba_Dünya
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Merhaba Dünya");
+			Console.Write("Adınız : ");
+			string Name = Console.ReadLine();
+			Console.WriteLine("Merhaba " + Name + " Galaksiye Hoş Geldin !");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Test-1 Merhaba Dünya/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-1 Merhaba Dünya")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-1 Merhaba Dünya")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cedbe1a6-c3e9-4211-be0b-2e6097e232e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
