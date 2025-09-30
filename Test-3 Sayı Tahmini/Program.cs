85bd5be95f871df7b21b4c8ee183c080f0eeaabc
Mehmet Salih <joker16131613@gmail.com>
2025-09-30 15:38:26 +0300

    Test-3

---
+++ Test-3 Sayı Tahmini/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test_3_Sayı_Tahmini
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" ------- Sayı Tahmin Oyunu ------- ");
+			Random randomNumber = new Random();
+			int rnd = randomNumber.Next(1, 10);
+			while (true)
+			{
+				Console.Write("Sayıyı Tahmin Edin : ");
+				int userResponse = Convert.ToInt32(Console.ReadLine());
+
+				if (userResponse == rnd)
+				{
+					Console.WriteLine("Tebrikler sayıyı doğru tahmin ettiniz.");
+					break;
+				}
+				else if (userResponse <= rnd)
+					Console.WriteLine("Tahmin ettiğiniz sayı küçük !!!");
+				else if (userResponse >= rnd)
+					Console.WriteLine("Tahmin ettiğiniz sayı büyük !!!");
+				else
+				{
+					Console.WriteLine("Geçersiz sayı !!!");
+				}
+			}
+		}
+	}
+}
