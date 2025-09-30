5f269f095a43ba8cf182fb27e2f597669da0dcae
Mehmet Salih <joker16131613@gmail.com>
2025-09-30 15:50:41 +0300

    Test-4

---
+++ Test-4 Kullanıcı Giriş Sistemi/LoginUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_4_Kullanıcı_Giriş_Sistemi
+{
+	public class LoginUserModel
+	{
+		public string UserName { get; set; }
+		public string UserPassword { get; set; }
+	}
+}
---
+++ Test-4 Kullanıcı Giriş Sistemi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_4_Kullanıcı_Giriş_Sistemi
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			LoginUserModel login = new LoginUserModel();
+			string userName = "Test-User";
+			string userPassword = "Test-User";
+
+			Console.WriteLine(" ---------- Kullanıcı Giriş Paneli ---------- ");
+
+			while (true)
+			{
+				Console.Write("Kullanıcı Adınız : ");
+				login.UserName = Console.ReadLine();
+				Console.Write("Kullanıcı Şifreniz : ");
+				login.UserPassword = Console.ReadLine();
+
+				if (login.UserName == userName && login.UserPassword == userPassword) 
+				{
+					Console.WriteLine("Giriş Başarılı.");
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Giriş Bilgilerin Yanlış !!!");
+				}
+			}
+		}
+	}
+}
