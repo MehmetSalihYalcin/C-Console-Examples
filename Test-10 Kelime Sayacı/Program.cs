aeb560cd282300c73307210773ee5e643f49d86b
Mehmet Salih <joker16131613@gmail.com>
2025-10-02 17:32:05 +0300

    Kullanıcıdan bir kelime al ve palindrom (tersten aynı) olup olmadığını kontrol et.

---
+++ Test-10 Kelime Sayacı/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_10_Kelime_Sayacı
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{  
+			Console.WriteLine(" ---------- Kelime Sayacı ---------- ");
+
+			while (true)
+			{
+				Console.Write("Metin giriniz : ");
+				string secenek = Console.ReadLine() ;
+				 var count = secenek.Split(' ');
+				Console.WriteLine("Kelime sayısı : " + count.Length);
+			}
+		}
+	}
+}
---
+++ Test-11 Palindrom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_11_Palindrom
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" ---------- Not Defteri ---------- ");
+
+			while (true)
+			{
+				Console.Write("Kelime giriniz : ");
+				string kelime = Console.ReadLine();
+				string tersHali = "";
+				char[] chars = kelime.ToLower().ToCharArray();
+
+				for (int i = chars.Length - 1; i > -1; i--)
+				{
+					tersHali = tersHali + chars[i];
+				}
+
+				if (kelime == tersHali)
+				{
+					Console.WriteLine(kelime + "Kelimesi palindrom");
+				}
+				else
+				{
+					Console.WriteLine(kelime + "Kelimesi palindrom değil");
+				}
+			}
+		}
+	}
+}
