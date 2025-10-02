47c203fecd3df45352b1e1976e4319deaafc2f6e
Mehmet Salih <joker16131613@gmail.com>
2025-10-02 11:02:37 +0300

    Kullanıcıdan bir sayı al ve asal olup olmadığını kontrol et.

---
+++ Test-8 Karekök Hesaplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_7_Karekök_Hesaplama
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" ---------- Karekök Hesaplama ---------- ");
+
+			while (true)
+			{
+				Console.Write("Sayı giriniz : ");
+				double number = Convert.ToInt32(Console.ReadLine());
+
+				double yaklasikDeger = number / 2;
+				double epsilon = 0.000001;
+
+				while(Math.Abs(yaklasikDeger * yaklasikDeger - number) > epsilon)
+				{
+					yaklasikDeger = (yaklasikDeger + number / yaklasikDeger) / 2;
+				}
+				Console.WriteLine(number + " sayısının karekökü: " + yaklasikDeger);
+			}
+		}
+	}
+}
---
+++ Test-8 Karekök Hesaplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-8 Karekök Hesaplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-8 Karekök Hesaplama")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("281f9418-f484-4454-a0ef-0f5d6b0d67e4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
