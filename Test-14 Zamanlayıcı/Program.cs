f3ba4944c89db3e8cad2c1b16f62973f3e53f75a
Mehmet Salih <joker16131613@gmail.com>
2025-10-03 11:24:32 +0300

    Kullanıcıdan saniye al ve geri sayım yap

---
+++ Test-14 Zamanlayıcı/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test_14_Zamanlayıcı
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" ---------- Zamanlayıcı ---------- \n\n");
+			while (true)
+			{
+				Console.Write("Geri sayma sayınızı giriniz : ");
+				int sayi = Convert.ToInt32(Console.ReadLine());
+
+				for (int i = sayi; i >= 0; i--)
+				{
+					 Thread.Sleep(1000);
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
