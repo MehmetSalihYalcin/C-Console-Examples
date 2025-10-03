c475625d6f22aec2cc883a1692501214b8bdf39f
Mehmet Salih <joker16131613@gmail.com>
2025-10-03 16:44:07 +0300

    Kullanıcıyla taş-kağıt-makas oyunu oyna, bilgisayarın seçimi rastgele olsun

---
+++ Test-16 Tas-Kagit-Makas/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_16_Tas_Kagit_Makas
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{ 
+			
+			while (true)
+			{
+				Random rnd = new Random();
+				var rastgele = rnd.Next(1,3);
+				Console.WriteLine(" ---------- Taş-Kağıt-Makas ---------- \n\n" +
+					"            1-)  Taş\n" +
+					"            2-) Kağıt\n" +
+					"            3-) Makas\n\n"
+					);
+
+				Console.Write("Yapmak İstediğiniz işlem : ");
+				int islem = Convert.ToInt32(Console.ReadLine());
+
+				if (islem == rastgele)
+				{
+					Console.WriteLine("Berabere");
+				}
+				else if (islem == 1)
+				{
+					if (rastgele != 2) Console.WriteLine(islem + " - "+ rastgele +" Sen kazandım ben kaybettin");
+					else Console.WriteLine(islem + " - " + rastgele + " Ben kazandım sen kaybettin");
+				}
+				else if (islem == 2)
+				{
+					if (rastgele == 1) Console.WriteLine(islem + " - " + rastgele + " Sen kazandım ben kaybettin");
+					else Console.WriteLine(islem + " - " + rastgele + " Ben kazandım sen kaybettin");
+				}
+				else if (islem == 3)
+				{
+					if (rastgele == 2) Console.WriteLine(islem + " - " + rastgele + " Sen kazandım ben kaybettin");
+					else Console.WriteLine(islem + " - " + rastgele + " Ben kazandım sen kaybettin");
+				}
+				else 
+				{
+					Console.WriteLine("Hatalı işlem !!!");
+				}
+			}
+		}
+	}
+}
