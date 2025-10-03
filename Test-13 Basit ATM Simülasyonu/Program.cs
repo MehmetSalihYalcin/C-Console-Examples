5190bfd2e24f30f1abd780b76e10334097e229be
Mehmet Salih <joker16131613@gmail.com>
2025-10-03 11:10:47 +0300

    Kullanıcıya bakiye göster, para yatırma/çekme işlemleri sun.

---
+++ Test-13 Basit ATM Simülasyonu/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_13_Basit_ATM_Simülasyonu
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int _banka = 0;
+			while (true)
+			{
+				Console.WriteLine(" ---------- Basit ATM Simülasyonu ---------- \n\n" +
+					"            1-) Güncel Bakiye\n" +
+					"            2-) Pare Çek\n" +
+					"            3-) Pare Yatır\n\n"
+					);
+
+				Console.Write("Yapmak İstediğiniz işlem : ");
+				int islem = Convert.ToInt32(Console.ReadLine());
+
+				if (islem == 1)
+				{
+					Console.WriteLine("Güncel Bakiyeniz : " + " "+_banka+" TL");
+				}
+				else if (islem == 2)
+				{
+					Console.WriteLine("Çekmek istediğiniz tutar : ");
+					int cek = Convert.ToInt32(Console.ReadLine());
+					_banka = _banka - cek; 
+					Console.WriteLine("\nGüncel Bakiyeniz : " + " " + _banka + " TL");
+				}
+				else if (islem == 3)
+				{
+					Console.WriteLine("Yatırmak istediğiniz tutar : ");
+					int yatir = Convert.ToInt32(Console.ReadLine());
+					_banka = _banka + yatir;
+					Console.WriteLine("\nGüncel Bakiyeniz : " + " " + _banka + " TL");
+				}
+				else
+				{
+					Console.WriteLine("Yanlış işlem !!!" );
+				}
+			}
+		}
+	}
+}
---
+++ Test-13 Basit ATM Simülasyonu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-13 Basit ATM Simülasyonu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-13 Basit ATM Simülasyonu")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7ce36856-3265-44d6-a7a2-c0fc95855bf9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
