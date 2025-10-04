90064d2ffd1f662617c9c084e29ba711896b1e85
Mehmet Salih <joker16131613@gmail.com>
2025-10-04 13:39:23 +0300

    Kullanıcıdan bir sayı al ve çarpım tablosunu yazdır.

---
+++ Test-19 Basit Çarpım Tablosu/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_19_Basit_Çarpım_Tablosu
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" ---------- Basit Çarpım Tablosu ---------- ");
+
+			while (true)
+			{
+				Console.Write("Sayı giriniz : ");
+				int sayi = Convert.ToInt32(Console.ReadLine());
+
+				for (int i = 1; i <= sayi; i++)
+				{
+					for (int j = 1; j <= sayi; j++)
+					{
+						Console.Write(i*j + "\t");
+					}
+					Console.WriteLine();
+				}
+			}
+		}
+	}
+}
