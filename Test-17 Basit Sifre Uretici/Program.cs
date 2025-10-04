e7e19014aa0d3849df5e4376507c770910c61ae9
Mehmet Salih <joker16131613@gmail.com>
2025-10-04 11:26:03 +0300

    Kullanıcıdan uzunluk al ve rastgele bir şifre üret.

---
+++ Test-17 Basit Sifre Uretici/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_17_Basit_Sifre_Uretici
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			object[] sifreKarakterleri = { "a", "A", "b", "B", "c", "C", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "Q", "q", "*", "/", "&" };//...
+
+			Console.WriteLine(" ---------- Basit Şifre Üretici ---------- ");
+			Random randomAl = new Random();
+			while (true)
+			{
+				string sifre = string.Empty;
+				Console.Write("\nŞifre uzunluğunu giriniz : ");
+				int sayi = Convert.ToInt32(Console.ReadLine());
+
+				for (int i = 0; i < sayi; i++)
+				{
+					var rnd = randomAl.Next(0, sifreKarakterleri.Length);
+					sifre = sifre + sifreKarakterleri[rnd].ToString();
+				}
+
+				Console.Write("Şifre : " + sifre);
+			}
+		}
+	}
+}
---
+++ Test-17 Basit Sifre Uretici/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-17 Basit Sifre Uretici")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-17 Basit Sifre Uretici")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b216294a-efa8-4d3f-a0fe-754f8f735b93")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
