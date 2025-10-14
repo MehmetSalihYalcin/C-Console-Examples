4f7bc5a09998e443c23c8d59e64dffbf90bbd999
Mehmet Salih <joker16131613@gmail.com>
2025-10-14 12:45:37 +0300

    Rastgele kelimelerden birini seç, harflerini karıştır ve kullanıcıdan doğru kelimeyi bulmasını iste.

---
+++ Test-28 Kelime Bulmaca Oyunu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_28_Kelime_Bulmaca_Oyunu
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] kelimeler = { "elma", "armut", "muz", "çilek", "karpuz", "kavun", "kiraz", "şeftali", "erik", "ayva" };
+			Random rnd = new Random();
+			while (true)
+			{
+				string tahmin = "";
+				var rastgele = rnd.Next(0, 9);
+				string kelime = kelimeler[rastgele];
+				char[] rastgeleKelimeHarfleri = new char[kelime.Length];
+								for (int i = 0; i < kelime.Length; i++)
+											{
+												int randomIndex;
+												do
+												{
+													randomIndex = rnd.Next(0, kelime.Length);
+												} while (rastgeleKelimeHarfleri[randomIndex] != '\0');
+
+												rastgeleKelimeHarfleri[randomIndex] = kelime[randomIndex];
+												tahmin += rastgeleKelimeHarfleri[randomIndex].ToString();
+								}
+				Console.WriteLine("Kelime : " + tahmin);
+				Console.Write("Kelimenin doğru yazılışını tahmin edin : ");
+				string cevap = Console.ReadLine();
+
+									if (cevap == kelime)
+									{
+												Console.WriteLine("Doğru tahmin");
+									}
+									else
+									{
+												Console.WriteLine("Yanlış");
+									}
+			}
+		}
+	}
+}
---
+++ Test-28 Kelime Bulmaca Oyunu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test-28 Kelime Bulmaca Oyunu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test-28 Kelime Bulmaca Oyunu")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3d7273b-e9cc-469a-86c0-ec6cfa67be64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
